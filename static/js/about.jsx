6ac74112f4b78fc370916eaf4176eec5c0363faf
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-10-12 19:51:01 -0700

    Finished /about endpoint

---
+++ static/js/about.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+function AboutMe() {
+  return (
+  <div>
+    This is the about me page.
+  </div>);
+}
+
+ReactDOM.render(<AboutMe />, document.querySelector('#about'));
--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -3,8 +3,13 @@
 function Homepage() {
   return (
   <div>
+    <div>
     <img src="/static/img/balloonicorn.jpg"></img>
     <a href="/cards">Head to the cards page</a>
+    </div>
+    <div>
+    <a href="/about">Head the about me section.</a>
+    </div>
   </div>);
 }
 
